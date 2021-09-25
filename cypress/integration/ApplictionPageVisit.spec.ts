9cf17b96b0e58e7c67ce4bb1caf0e60365b99c53
RAJNI\Rajni <rajni0912@gmail.com>
2021-09-25 17:56:30 +1000

    Adding cooments to functions

--- cypress/integration/ApplictionPageVisit.spec.ts
+++ cypress/integration/ApplictionPageVisit.spec.ts
@@ -8,22 +8,31 @@ context('AppTest', () => {
         appPage.visit()
     })
 
-    it('[01] Visit the Page', () => {
+    it('[01] Test: Visit the Page', () => {
         // Check Article Headline
          appPage.articleHeadline().should("have.text", "Capability edge and keeping South China Sea open crucial: Christopher Pyne ");
     })
 
-    it('[02] Check Subscription Prompt Visible', () => {
+    it('[02] Test: Subscription Prompt Visible', () => {
         // Check if Subscription Prompt is visible
-        appPage.getActivePopup().should("be.visible");
+        appPage.getActiveSubscriptionPrompt().should("be.visible");
         appPage.getSubscriptionPromptTitle().should("have.text","Save 50% for your first 3 months.");
     })
 
-    it('[03] Scroll to End of Page', () => {
+    it('[03] Test: Scroll to End of Page', () => {
         // wait 10 seconds for the pop-up window to close
-        cy.wait(10000);
+        //cy.wait(10000);
         cy.scrollTo("bottom");
+        // check if Footer is visible and Site Map link is there.
         appPage.getPageFooter().should("be.visible");
         appPage.getSiteMapElement().should("have.text", "Site Map")
+    })
+
+    it('[04] Test: Subscription Prompt not visible after 10 seconds ', () => {
+        // wait 10 seconds for the pop-up window to close
+        cy.wait(10000);
+        // check if the data-testid value = SubscriptionPrompt-false
+        appPage.getInactiveSubscriptionPrompt().should('be.visible');
+
     })
 })
--- cypress/pages/AppPage.ts
+++ cypress/pages/AppPage.ts
@@ -2,32 +2,33 @@
 
 class AppPage {
 
-    constructor() {
-
-    }
+    constructor() {} 
+    // Visiting the page
     visit() {
         const siteURL = "https://www.afr.com/policy/foreign-affairs/capability-edge-and-keeping-south-china-sea-open-crucial--christopher-pyne-20180924-h15rq9";
         return cy.visit(siteURL, { failOnStatusCode: true, timeout: 40000 });
     }
-
+    // checking the article headline
     articleHeadline() {
         return cy.get('[data-testid="ArticleHeader-headline"]');
     }
-
-    getActivePopup() {
+    // checking the subscription prompt pop up
+    getActiveSubscriptionPrompt() {
         return cy.get('[data-testid="SubscriptionPrompt-true"]');
     }
-
+    // Checking the subscription prompt pop up title
     getSubscriptionPromptTitle() {
         return cy.get('[data-testid="SubscriptionPrompt-title"]');
     }
-    getInactivePopup() {
+    // Check if the subscription prompt disappears
+    getInactiveSubscriptionPrompt() {
         return cy.get('[data-testid="SubscriptionPrompt-false"]');
     }
-
+    //Getting page footer
     getPageFooter() {
         return cy.get("#footer");
     }
+    // getting the sitemap link at the bottom of the page
     getSiteMapElement() {
         return cy.get('._27qNq');
     }
