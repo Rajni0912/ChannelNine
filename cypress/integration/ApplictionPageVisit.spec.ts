e67a6c06e39132402123696e101de47ed49fdcf1
RAJNI\Rajni <rajni0912@gmail.com>
2021-09-25 17:17:34 +1000

    Test cases for Scenarios

--- cypress/integration/ApplictionPageVisit.spec.ts
+++ cypress/integration/ApplictionPageVisit.spec.ts
@@ -10,6 +10,20 @@ context('AppTest', () => {
 
     it('[01] Visit the Page', () => {
         // Check Article Headline
-         appPage.pageHeader().should("have.text", "Capability edge and keeping South China Sea open crucial: Christopher Pyne ");
+         appPage.articleHeadline().should("have.text", "Capability edge and keeping South China Sea open crucial: Christopher Pyne ");
+    })
+
+    it('[02] Check Subscription Prompt Visible', () => {
+        // Check if Subscription Prompt is visible
+        appPage.getActivePopup().should("be.visible");
+        appPage.getSubscriptionPromptTitle().should("have.text","Save 50% for your first 3 months.");
+    })
+
+    it('[03] Scroll to End of Page', () => {
+        // wait 10 seconds for the pop-up window to close
+        cy.wait(10000);
+        cy.scrollTo("bottom");
+        appPage.getPageFooter().should("be.visible");
+        appPage.getSiteMapElement().should("have.text", "Site Map")
     })
 })
--- cypress/pages/AppPage.ts
+++ cypress/pages/AppPage.ts
@@ -1,26 +1,35 @@
 /// <reference types="cypress" />
 
 class AppPage {
-    
+
     constructor() {
-        
-     }
+
+    }
     visit() {
-        const siteURL =  "https://www.afr.com/policy/foreign-affairs/capability-edge-and-keeping-south-china-sea-open-crucial--christopher-pyne-20180924-h15rq9";
+        const siteURL = "https://www.afr.com/policy/foreign-affairs/capability-edge-and-keeping-south-china-sea-open-crucial--christopher-pyne-20180924-h15rq9";
         return cy.visit(siteURL, { failOnStatusCode: true, timeout: 40000 });
     }
 
     articleHeadline() {
-
-        return cy.get('[data-testid="ArticleHeader-headline"]')
-        
+        return cy.get('[data-testid="ArticleHeader-headline"]');
     }
 
     getActivePopup() {
+        return cy.get('[data-testid="SubscriptionPrompt-true"]');
+    }
 
+    getSubscriptionPromptTitle() {
+        return cy.get('[data-testid="SubscriptionPrompt-title"]');
     }
     getInactivePopup() {
+        return cy.get('[data-testid="SubscriptionPrompt-false"]');
+    }
 
+    getPageFooter() {
+        return cy.get("#footer");
+    }
+    getSiteMapElement() {
+        return cy.get('._27qNq');
     }
 }
 
